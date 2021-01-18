6eac21dc4746e77d6bbc9fefbc7aeaf4b10c6a61
Florian Wessels <flo.wessels@gmail.com>
2021-01-18 13:39:01 +0100

    [TASK] Set version to 1.2.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,15 +3,8 @@
 $EM_CONF['audience_studio'] = [
     'title' => 'Marketing Automation - Audience Studio Adapter',
     'description' => 'Add-on TYPO3 extension that enhances the "EXT:marketing_automation" TYPO3 extension by connecting it to Salseforce Audience Studio',
+    'version' => '1.2.0-dev',
     'category' => 'fe',
-    'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'author' => 'Florian Wessels, Helmut Hummel',
-    'author_company' => 'Leuchtfeuer Digital Marketing',
-    'author_email' => 'dev@Leuchtfeuer.com',
-    'clearCacheOnLoad' => 0,
-    'version' => '1.1.1',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.2-10.4.99',
@@ -19,6 +12,18 @@ $EM_CONF['audience_studio'] = [
         ],
         'conflicts' => [],
         'suggests' => [],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Florian Wessels, Helmut Hummel',
+    'author_company' => 'Leuchtfeuer Digital Marketing',
+    'author_email' => 'dev@Leuchtfeuer.com',
+    'autoload' => [
+        'psr-4' => [
+            'Leuchtfeuer\\Typo3AudienceStudio\\' => 'Classes',
+        ],
     ],
 ];
 
