798a0f074b66f77d2a313d0a0e20ba791e637791
Florian Wessels <flo.wessels@gmail.com>
2021-01-18 13:42:16 +0100

    [FEATURE] Allow installation on non composer setups

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,8 +2,12 @@
 defined('TYPO3_MODE') || die;
 
 call_user_func(static function () {
+    // Load libraries when TYPO3 is not in composer mode
+    // TODO: Use environment class when dropping TYPO3 v9 support.
+    if (!defined('TYPO3_COMPOSER_MODE') || !TYPO3_COMPOSER_MODE) {
+        require \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('audience_studio') . 'Libraries/vendor/autoload.php';
+    }
 
-    $marketingDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Bitmotion\MarketingAutomation\Dispatcher\Dispatcher::class);
-    $marketingDispatcher->addSubscriber(\Leuchtfeuer\Typo3AudienceStudio\MarketingAutomation\AudienceStudioSubscriber::class);
-
+    \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Bitmotion\MarketingAutomation\Dispatcher\Dispatcher::class)
+        ->addSubscriber(\Leuchtfeuer\Typo3AudienceStudio\MarketingAutomation\AudienceStudioSubscriber::class);
 });
